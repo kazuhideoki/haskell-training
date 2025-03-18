d2e73951ea046cc88de232d09804bc4f015576e7
Kazuhide Oki <cedar123pc@gmail.com>
2025-03-19 08:50:32 +0900

    solve long

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  arg <- readLn :: IO Int
+  let result = exe arg
+  print result
+
+-- 文字列 `o` を引数分挿入する Loooong
+exe :: Int -> String
+exe n = "L" ++ replicate n 'o' ++ "ng"
