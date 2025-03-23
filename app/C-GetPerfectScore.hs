a5c8400eeeca46cd729768d9abd290d1bc233243
Kazuhide Oki <cedar123pc@gmail.com>
2025-03-23 15:59:35 +0900

    solve E - World Tour Finals

---
+++ app/C-GetPerfectScore.hs
@@ -0,0 +1,32 @@
+-- C - Let's Get a Perfect Score
+-- https://atcoder.jp/contests/adt_easy_20250319_3/tasks/abc282_b
+module Main where
+
+import Control.Monad (replicateM)
+import Data.Function ((&))
+
+main :: IO ()
+main = do
+  line <- getLine
+  let intList = line & words & map read :: [Int]
+      numOfAnswers = head intList
+  answers <- replicateM numOfAnswers getLine
+
+  let result = numOfGettingCorrectAnswersByTwo answers
+
+  print result
+
+numOfGettingCorrectAnswersByTwo :: [String] -> Int
+numOfGettingCorrectAnswersByTwo answers =
+  pairs answers & filter canSolve & length
+
+-- 重複しないようなペアを生成する
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs (x : xs) = [(x, y) | y <- xs] ++ pairs xs
+
+-- 'o' が含まれるペアを生成する
+canSolve :: (String, String) -> Bool
+canSolve (x, y) =
+  let charPairs = zip x y
+   in all (\(a, b) -> a == 'o' || b == 'o') charPairs
--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,62 @@
--- C - Let's Get a Perfect Score
--- https://atcoder.jp/contests/adt_easy_20250319_3/tasks/abc282_b
+-- E - World Tour Finals
+-- https://atcoder.jp/contests/adt_easy_20250319_3/tasks/abc323_c
 module Main where
 
 import Control.Monad (replicateM)
 import Data.Function ((&))
+import Data.List (sortBy)
 
 main :: IO ()
 main = do
-  line <- getLine
-  let intList = line & words & map read :: [Int]
-      numOfAnswers = head intList
-  answers <- replicateM numOfAnswers getLine
+  line1 <- getLine
+  line2 <- getLine
+  let list1 = line1 & words & map read :: [Int]
+      playerCount = head list1
+      _problemCount = head $ tail list1
+      scores = line2 & words & map read :: [Int]
+  progresses <- replicateM playerCount getLine
+  let topScore = maximum $ zipWith (`calcScore` scores) progresses [0 ..]
+  let counts =
+        zipWith
+          ( \progress bonus ->
+              calcMinimumRequiredSolving progress scores topScore bonus
+          )
+          progresses
+          [0 ..]
+   in do
+        mapM_ print counts
 
-  let result = numOfGettingCorrectAnswersByTwo answers
+toBools :: [String] -> [Bool]
+toBools = map (== "o")
 
-  print result
+-- 現在の最高得点を取得 (progress -> scores -> bounusScore)
+calcScore :: String -> [Int] -> Int -> Int
+calcScore _ [] _ = 0
+calcScore [] _ _ = 0
+calcScore xs ys bonus =
+  zip xs ys
+    & filter (\(x, _) -> x == 'o')
+    & map snd
+    & sum
+    & (+) bonus
 
-numOfGettingCorrectAnswersByTwo :: [String] -> Int
-numOfGettingCorrectAnswersByTwo answers =
-  pairs answers & filter canSolve & length
-
--- 重複しないようなペアを生成する
-pairs :: [a] -> [(a, a)]
-pairs [] = []
-pairs (x : xs) = [(x, y) | y <- xs] ++ pairs xs
-
--- 'o' が含まれるペアを生成する
-canSolve :: (String, String) -> Bool
-canSolve (x, y) =
-  let charPairs = zip x y
-   in all (\(a, b) -> a == 'o' || b == 'o') charPairs
+-- トップを越えるために必要な最低の正解数を取得
+-- (progress -> scores -> topScore -> bonusScore -> 正解数)
+-- 1. 正解してない問題からスコアの大きい順に足していく
+-- 2. スコアがrequiedScore に達したら足された数が出力
+calcMinimumRequiredSolving :: String -> [Int] -> Int -> Int -> Int
+calcMinimumRequiredSolving progress scores topScore bonusScore =
+  let currentScore = calcScore progress scores bonusScore
+      notSolvingScores = zip progress scores & filter (\(x, _) -> x == 'x') & map snd & sortBy (flip compare)
+   in if currentScore == topScore
+        then 0
+        else
+          snd $
+            foldl
+              ( \(totalScore, count) score ->
+                  if totalScore <= topScore
+                    then (totalScore + score, count + 1)
+                    else (totalScore, count)
+              )
+              (currentScore, 0)
+              notSolvingScores
