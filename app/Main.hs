b6e95fa8280ac8b832824f88f6e54d4c5ea3b644
Kazuhide Oki <cedar123pc@gmail.com>
2025-03-30 07:43:06 +0900

    wip -- C - Make it Forest

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,16 @@
--- B - Ranking with Ties
--- https://atcoder.jp/contests/abc399/tasks/abc399_b
+-- C - Make it Forest
+-- https://atcoder.jp/contests/abc399/tasks/abc399_c
 module Main where
 
-import Data.Function ((&))
+import Control.Monad (replicateM)
 import Data.Functor ((<&>))
 
 -- sort する -> 順位つける -> 戻す
 main :: IO ()
 main = do
-  -- 例: 4
-  n <- (getLine <&> read) :: IO Int
-  -- 例: [1, 2, 3, 4]
-  s <- (getLine <&> map read . words) :: IO [Int]
-  let initialPoints = zip s (replicate n 0)
-      (_, result) = decideRank (1, initialPoints)
-      ranks = map (show . snd) result & unwords
-   in putStrLn ranks
-
--- r -> (point, rank) -> (point, rank)
-decideRank :: (Int, [(Int, Int)]) -> (Int, [(Int, Int)])
-decideRank (r, points) =
-  let maxPoint = points & filter (\(_, rank) -> rank == 0) & map fst & maximum
-      result = points & map (\(point, rank) -> if point == maxPoint then (point, r) else (point, rank))
-      newR = result & filter ((/= 0) . snd) & length & (+) 1
-   in if newR > length points then (newR, result) else decideRank (newR, result)
+  -- 例: [10, 10] [頂点, 辺の数]
+  [n, m] <- (getLine <&> map read . words) :: IO [Int]
+  -- 例: [[1, 2], [3, 4], [5, 6]] [辺の始点, 辺の終点]
+  vertexes <- replicateM m (getLine <&> map read . words) :: IO [[Int]]
+  print [n, m]
+  print vertexes
